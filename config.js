5ee790dc27fe8b61eadac6ec7d90310b955e0a00
sanijanimofc <fhghfddf83@gmail.com>
2025-06-05 19:25:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
      return text === fault ? true : false;
  }
  module.exports = {
- SESSION_ID: process.env.SESSION_ID || "SANIJA-MD=qApXUS4b#kmCV2aDP_Iuub8bX3DQxETrtFJYgD7aXOFx3rRhgorU",
+ SESSION_ID: process.env.SESSION_ID || "SANIJA-MD=jZ81AKZD#IoolxmZxtKeeUsoSmGUSIN0nRn3ZGEqFpUw95LdNrVE",
  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
  MODE: process.env.MODE || "private",
  AUTO_VOICE: process.env.AUTO_VOICE || "false",
