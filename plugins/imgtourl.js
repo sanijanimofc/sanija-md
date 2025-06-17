55b823e79a55898c76fad12ab6dd14c1a794186f
sanijanimofc <fhghfddf83@gmail.com>
2025-06-17 18:50:38 +0530

    Create imgtourl.js

---
+++ plugins/imgtourl.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const {cmd , commands} = require('../command');
+
+cmd({
+    pattern: "tourl",
+    alias: ["imgurl","img2url"],
+    react: '♻',
+    desc: "Download anime maid images.",
+    category: "anime",
+    use: '.maid',
+    filename: __filename
+},
+async(conn, mek, m, {from, mnu, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+ 
+try{
+
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) throw `_\`img එකට රිප්ලයි කර🔷\`_`;
+ // if (!args[0]) throw ` \`\`\`[ 🌺 ] Ingresa un texto para guardar la imagen. Ejemplo:\n${usedPrefix + command} Sylph\`\`\``
+
+  let media = await q.download();
+  let tempFilePath = path.join(os.tmpdir(), 'SulaMd');
+  fs.writeFileSync(tempFilePath, media);
+
+  let form = new FormData();
+  form.append('image', fs.createReadStream(tempFilePath));
+
+    let response = await axios.post('https://api.imgbb.com/1/upload?key=02b01525bdac411947ab8d1e2cd90a68', form, {
+      headers: {
+        ...form.getHeaders()
+      }
+    });
+
+    if (!response.data || !response.data.data || !response.data.data.url) throw '❌ Error al subir el archivo';
+    
+    let link = response.data.data.url;
+    fs.unlinkSync(tempFilePath);
+
+    m.reply(`SANIJA MD IMG TO URL\n\n*File Size* ${media.length} *Byte(s)*\n\n*IMG URL* ${link}\n\n> Powered By SANIJA MD 😈`);
+    
+} catch (e) {
+reply(`${e}`)
+console.log(e)
+}
+})
